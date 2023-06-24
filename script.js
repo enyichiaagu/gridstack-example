3ce3b125170f513c91856b984bf0c5958f8c863f
Amazing Agu <enyichiaagu@gmail.com>
2023-06-24 14:14:09 +0100

    Playing around with options

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-GridStack.init()
+const serializedData = [
+	{ x: 0, y: 0, w: 2, h: 2 },
+	{ x: 0, y: 2, w: 3, content: 'item 2' },
+	{ x: 0, y: 3 },
+]
+
+const options = {
+	styleInHead: false,
+	margin: 5,
+	resizable: {
+		handles: 'e, se, s, sw, w'
+	}
+}
+
+const grid = GridStack.init(options)
+grid.load(serializedData)
